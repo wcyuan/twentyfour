9d82634cb9b39e1ce320b656b48a50182113eb42
Conan Yuan <wcy@wcy-macbookair.roam.corp.google.com>
2016-04-01 00:48:07 -0400

    Several improvements
     - add inputs so you can specify the values and target
     - make functions public, but put them in the tf namespace
     - move code link to the bottom
     - make hidden answers take no space (display = none instead of
       visibility = hidden)

--- tf.js
+++ tf.js
@@ -1,4 +1,3 @@
-
 // make sure we've got namespace
 if (typeof wd == "undefined") {
   var wd = {};
@@ -90,102 +89,115 @@ Function.prototype.bind = function(obj) {
 
 // ------------------------------------------------------------------ //
 
-(function() {
+if (typeof tf == "undefined") {
+  var tf = {};
+}
 
-    function permutations(vals) {
-        if (vals.length < 2) {
-            return [vals.slice(0)];
-        }
-        var perms = [];
-        var first = vals[0];
-        permutations(vals.slice(1)).forEach(function(subresult) {
-            for (var ii = 0; ii <= subresult.length; ii++) {
-                var newarr = subresult.slice(0);
-                newarr.splice(ii, 0, first);
-                perms.push(newarr);
-            }
-        });
-        return perms;
+tf.permutations = function(vals) {
+    if (vals.length < 2) {
+        return [vals.slice(0)];
     }
+    var perms = [];
+    var first = vals[0];
+    tf.permutations(vals.slice(1)).forEach(function(subresult) {
+        for (var ii = 0; ii <= subresult.length; ii++) {
+            var newarr = subresult.slice(0);
+            newarr.splice(ii, 0, first);
+            perms.push(newarr);
+        }
+    });
+    return perms;
+};
 
-    function partitions(vals) {
-        if (vals.length < 2) {
-            return [];
-        }
-        var parts = [];
-        for (var ii = 1; ii < vals.length; ii++) {
-            parts.push([vals.slice(0, ii), vals.slice(ii)]);
-        }
-        return parts;
+tf.partitions = function(vals) {
+    if (vals.length < 2) {
+        return [];
     }
+    var parts = [];
+    for (var ii = 1; ii < vals.length; ii++) {
+        parts.push([vals.slice(0, ii), vals.slice(ii)]);
+    }
+    return parts;
+};
 
 
-    function list_to_exprs(vals) {
-        if (vals.length < 2) {
-            return vals;
-        }
-        var operations = [" + ", " - ", " * ", " / "];
-        var exprs = [];
-        partitions(vals).forEach(function(partition) {
-            list_to_exprs(partition[0]).forEach(function(left) {
-                list_to_exprs(partition[1]).forEach(function(right) {
-                    operations.forEach(function(op) {
-                        exprs.push("(" + left + op + right + ")");
-                    });
+tf.list_to_exprs = function(vals) {
+    if (vals.length < 2) {
+        return vals;
+    }
+    var operations = [" + ", " - ", " * ", " / "];
+    var exprs = [];
+    tf.partitions(vals).forEach(function(partition) {
+        tf.list_to_exprs(partition[0]).forEach(function(left) {
+            tf.list_to_exprs(partition[1]).forEach(function(right) {
+                operations.forEach(function(op) {
+                    exprs.push("(" + left + op + right + ")");
                 });
             });
         });
-        return exprs;
-    }
+    });
+    return exprs;
+};
 
 
-    function find_exprs(vals, target) {
-        var matches = [];
-        permutations(vals).forEach(function(perm) {
-            list_to_exprs(perm).forEach(function(expr) {
-                if (eval(expr) == target) {
-                    matches.push(expr);
-                }
-            });
+tf.find_exprs = function(vals, target) {
+    var matches = [];
+    tf.permutations(vals).forEach(function(perm) {
+        tf.list_to_exprs(perm).forEach(function(expr) {
+            if (eval(expr) == target) {
+                matches.push(expr);
+            }
         });
-        return matches;
+    });
+    return matches;
+};
+
+tf.isNumeric = function(n) {
+    return !isNaN(parseFloat(n)) && isFinite(n);
+}
+
+tf.randint = function(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+};
+
+tf.randnums = function(min, max, number) {
+    var vals = [];
+    for (var ii = 0; ii < number; ii++) {
+        vals.push(tf.randint(min, max));
+    }
+    return vals;
+};
+
+tf.reload = function() {
+    var exprs = [];
+    var vals;
+    while (exprs.length < 1) {
+        vals = tf.randnums(1, 13, 4);
+        exprs = tf.find_exprs(vals, 24);
+    }
+    tf.display(vals, 24, exprs);
+};
+
+tf.display = function(vals, target, exprs) {
+
+    if (typeof exprs == "undefined") {
+        exprs = tf.find_exprs(vals, target);
     }
 
-    function randint(min, max) {
-        return Math.floor(Math.random() * (max - min + 1)) + min;
-    }
+    var numbers = document.getElementById("numbers");
+    var num_answers = document.getElementById("num_answers");
+    var answers = document.getElementById("answers");
+    wd.removeClass(answers, "shown")
+    wd.addClass(answers, "hidden")
 
-    function randnums(min, max, number) {
-        var vals = [];
-        for (var ii = 0; ii < number; ii++) {
-            vals.push(randint(min, max));
-        }
-        return vals;
-    }
+    numbers.innerHTML = "<pre>" + vals + "</pre>";
+    num_answers.innerHTML = "<pre>(" + exprs.length + " answers found)</pre>";
+    answers.innerHTML = "<pre>" + exprs.join("\n") + "</pre>";
+};
 
-    // ------------------------------------------------------------------ //
+// ------------------------------------------------------------------ //
 
-    function reload() {
-        var exprs = [];
-        var vals;
-        while (exprs.length < 1) {
-            vals = randnums(1, 12, 4);
-            exprs = find_exprs(vals, 24);
-        }
-        var numbers = document.getElementById("numbers");
-        var num_answers = document.getElementById("num_answers");
-        var answers = document.getElementById("answers");
-        wd.removeClass(answers, "shown")
-        wd.addClass(answers, "hidden")
-
-        numbers.innerHTML = "<pre>" + vals + "</pre>";
-        num_answers.innerHTML = "<pre>(" + exprs.length + " answers found)</pre>";
-        answers.innerHTML = "<pre>" + exprs.join("\n") + "</pre>";
-    }
-
-    reload();
-
-    // ------------------------------------------------------------------ //
+(function() {
 
     var show_button = document.getElementById("show");
     wd.addEventListener(show_button, 'click', function() {
@@ -200,6 +212,26 @@ Function.prototype.bind = function(obj) {
     });
 
     var reload_button = document.getElementById("reload");
-    wd.addEventListener(reload_button, 'click', reload);
+    wd.addEventListener(reload_button, 'click', tf.reload);
+
+    var solve_button = document.getElementById("solve");
+    wd.addEventListener(solve_button, 'click', function() {
+	    var vals = [];
+	    ["a", "b", "c", "d"].forEach(function(id) {
+		    var input = document.getElementById(id);
+		    if (tf.isNumeric(input.value)) {
+			vals.push(input.value);
+		    }
+		});
+	    var target = document.getElementById("target").value;
+	    if (!tf.isNumeric(target)) {
+		target = 24;
+	    }
+	    if (vals.length > 0) {
+		tf.display(vals, target);
+	    }
+	});
+
+    tf.reload();
 
 }());
